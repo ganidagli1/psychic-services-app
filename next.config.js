cfa0dcc5be6f8bc72f13711b5e9455d0515d9f3e
ganidagli1 <gamzelim@gmail.com>
2025-10-07 07:00:46 +0100

    Add next.config.js with configuration for image domains for Google and Facebook OAuth providers.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['google.com', 'facebook.com'],
+  },
+  // Additional Next.js configurations can be added here
+}
+
+module.exports = nextConfig;
